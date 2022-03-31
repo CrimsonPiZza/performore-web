73e87d8e52dd2bc4cc0eebfabdb7961be52edacf
Hourn Sovannara <hournsovannara18@kit.edu.kh>
2022-03-31 21:05:06 +0700

    feat: enhance video playing controls
    allow pause on click

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -65,3 +65,18 @@ function pauseVideo() {
   playBtn.classList.remove("playing");
   video.pause();
 }
+
+function toggleVideo() {
+  if (
+    !!(
+      video.currentTime > 0 &&
+      !video.paused &&
+      !video.ended &&
+      video.readyState > 2
+    )
+  ) {
+    pauseVideo();
+  } else {
+    playVideo();
+  }
+}
